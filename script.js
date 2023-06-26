e00a22e689ccf7124af7f9332a4cd3246afef6a8
lucasbustos1993 <97643711+lucasbustos1993@users.noreply.github.com>
2023-06-25 23:26:58 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,224 @@
+$(function() {
+  $("#datepicker").datepicker({
+    beforeShowDay: function(date) {
+      // Obtener la fecha en formato YYYY-MM-DD
+      var formattedDate = $.datepicker.formatDate("yy-mm-dd", date);
+      
+      // Verificar si hay un valor almacenado para la fecha seleccionada
+      var horasExtras = localStorage.getItem("horasExtras_" + formattedDate);
+      
+      // Determinar la clase CSS a aplicar al día
+      var cssClass = (horasExtras !== null) ? "highlight-day" : "";
+      
+      // Retornar un array con la clase CSS y la indicación de si el día está disponible o no
+      return [true, cssClass];
+    },
+    onSelect: function(dateText, inst) {
+      // Obtener el valor almacenado para la fecha seleccionada
+      var formattedDate = $.datepicker.formatDate("yy-mm-dd", new Date(dateText));
+      var horasExtras = localStorage.getItem("horasExtras_" + formattedDate);
+      
+      if (horasExtras !== null) {
+        // Mostrar el valor almacenado en pantalla
+        document.getElementById("valorGuardado").textContent = "Valor guardado: " + horasExtras + " horas";
+        document.getElementById("borrar").style.display = "inline"; // Mostrar el botón "Borrar"
+      } else {
+        // Limpiar el valor guardado si no hay un valor almacenado para la fecha seleccionada
+        document.getElementById("valorGuardado").textContent = "";
+        document.getElementById("borrar").style.display = "none"; // Ocultar el botón "Borrar"
+      }
+      
+      
+    }
+  }).datepicker("show"); // Mostrar el calendario al cargar la página
+  // Llamar a la función actualizarHorasAcumuladas() después de cargar la página
+  actualizarHorasAcumuladas();
+});
+
+function calcularHoras() {
+  
+  var fechaSeleccionada = $("#datepicker").datepicker("getDate");
+  
+  if (fechaSeleccionada) {
+    var diaSemana = fechaSeleccionada.getDay();
+    if (diaSemana === 0) {
+      alert("No se pueden guardar valores para un día no laboral (domingo).");
+      return;
+    } else if (diaSemana === 6){
+      siSabado();
+    } else if (diaSemana > 0 && diaSemana < 6) {
+      siSemana();
+    } else {
+      alert("Selecciona una fecha");
+    }
+  }
+  actualizarHorasAcumuladas(totalHoras80, totalHoras100);
+}
+
+function reiniciarMes() {
+  // Limpiar el LocalStorage
+  localStorage.clear();
+
+  // Actualizar la interfaz después de limpiar el LocalStorage
+  document.getElementById("valorGuardado").textContent = "";
+  totalHoras100 = 0;
+  totalHoras80 = 0;
+
+  // Recorrer todas las claves del almacenamiento local
+  for (var i = 0; i < localStorage.length; i++) {
+    var clave = localStorage.key(i);
+    
+    // Verificar si la clave es de horas extras
+    if (clave.startsWith("horasExtras_")) {
+      // Eliminar la clave del almacenamiento local
+      localStorage.removeItem(clave);
+    }
+  }
+
+  // Remover estilos de resaltado en el calendario
+  $(".ui-datepicker-calendar a.ui-state-highlight").removeClass("ui-state-highlight");
+
+  // Actualizar el calendario para reflejar los cambios
+  $("#datepicker").datepicker("refresh");
+  actualizarHorasAcumuladas(0, 0);
+
+}
+
+
+
+function eliminar() {
+  var formattedDate = $.datepicker.formatDate("yy-mm-dd", $("#datepicker").datepicker("getDate"));
+  localStorage.removeItem("horasExtras_" + formattedDate);
+  document.getElementById("valorGuardado").textContent = ""; // Limpiar el valor guardado en pantalla
+  document.getElementById("borrar").style.display = "none"; // Ocultar el botón "Borrar"
+}
+
+function valorHora() {
+  var hora80 = parseFloat(document.getElementById("abonado80").value);
+  var cantidad80 = parseFloat(document.getElementById("cantidad80").value);
+  var valor80 = (hora80 / cantidad80).toFixed(2);
+  document.getElementById("valorHora80").textContent = "Valor hora extra 80%: " + valor80;
+
+  var hora100 = parseFloat(document.getElementById("abonado100").value);
+  var cantidad100 = parseFloat(document.getElementById("cantidad100").value);
+  var valor100 = (hora100 / cantidad100).toFixed(2);
+  document.getElementById("valorHora100").textContent = "Valor hora extra 100%: " + valor100;
+
+  var abonado80 = totalHoras80;
+  var abonado100 = totalHoras100;
+
+  var enMano = ((abonado80 * valor80) + (abonado100 * valor100)).toFixed(2);
+  document.getElementById("enMano").textContent = "Monto a cobrar en mano: " + enMano;
+
+  document.getElementById("resultados").style.display = "block";
+}
+
+
+var totalHoras100 = 0;
+var totalHoras80 = 0;
+
+  
+function actualizarHorasAcumuladas() {
+  // Mostrar las horas extras acumuladas en pantalla
+  document.getElementById("mostrar80").textContent = totalHoras80;
+  console.log(totalHoras80);
+  document.getElementById("mostrar100").textContent = totalHoras100;
+  console.log(totalHoras100);
+}
+
+function siSabado() {
+  var horaSalida = document.getElementById("horaSalida").value;
+  var fechaSeleccionada = $("#datepicker").datepicker("getDate");
+  var limiteHoraExtraSabado = [
+    [12, 14, 0.5],
+    [12, 44, 1],
+    [13, 14, 1.5],
+    [13, 44, 2],
+    [14, 14, 2.5],
+    [14, 44, 3]
+  ];
+
+  var fechaHoraSalida = new Date(fechaSeleccionada);
+  fechaHoraSalida.setHours(parseInt(horaSalida.substr(0, 2)));
+  fechaHoraSalida.setMinutes(parseInt(horaSalida.substr(3, 2)));
+
+  var horasExtraSabado = 0;
+  
+
+  for (var i = 0; i < limiteHoraExtraSabado.length; i++) {
+    var limiteHoraExtra = new Date(fechaSeleccionada);
+    limiteHoraExtra.setHours(limiteHoraExtraSabado[i][0]);
+    limiteHoraExtra.setMinutes(limiteHoraExtraSabado[i][1]);
+    
+
+    if (fechaHoraSalida > limiteHoraExtra) {
+      horasExtraSabado = limiteHoraExtraSabado[i][2];
+      
+    } else {
+      break;
+    }
+  }
+
+  var formattedDate = $.datepicker.formatDate("yy-mm-dd", fechaSeleccionada);
+  var horasGuardadas = parseFloat(localStorage.getItem("horasExtras_" + formattedDate)) || 0;
+
+  if (horasGuardadas === 0) {
+    localStorage.setItem("horasExtras_" + formattedDate, horasExtraSabado);
+    totalHoras100 += horasExtraSabado;
+    document.getElementById("valorGuardado").textContent = "Valor guardado: " + horasExtraSabado + " horas";
+    document.getElementById("borrar").style.display = "inline";
+    
+  } else {
+    alert("Ya existe un valor guardado para este día.");
+  }
+}
+
+
+function siSemana() {
+  
+  var horaSalida = document.getElementById("horaSalida").value;
+  var fechaSeleccionada = $("#datepicker").datepicker("getDate");
+  var limiteHoraExtra = [
+    [15, 14, 0.5],
+    [15, 44, 1],
+    [16, 14, 1.5],
+    [16, 44, 2],
+    [17, 14, 2.5],
+    [17, 44, 3]
+  ];
+
+  var fechaHoraSalida = new Date(fechaSeleccionada);
+  fechaHoraSalida.setHours(parseInt(horaSalida.substr(0, 2)));
+  fechaHoraSalida.setMinutes(parseInt(horaSalida.substr(3, 2)));
+
+  var horasExtras = 0;
+  
+  for (var i = 0; i < limiteHoraExtra.length; i++) {
+    var limiteHoraExtraTemp = new Date(fechaSeleccionada);
+    limiteHoraExtraTemp.setHours(limiteHoraExtra[i][0]);
+    limiteHoraExtraTemp.setMinutes(limiteHoraExtra[i][1]);
+
+    if (fechaHoraSalida > limiteHoraExtraTemp) {
+      horasExtras = limiteHoraExtra[i][2];
+      
+    } else {
+      break;
+    }
+  }
+
+  var formattedDate = $.datepicker.formatDate("yy-mm-dd", fechaSeleccionada);
+  var horasGuardadas = parseFloat(localStorage.getItem("horasExtras_" + formattedDate)) || 0;
+
+  if (horasGuardadas === 0) {
+    localStorage.setItem("horasExtras_" + formattedDate, horasExtras);
+    totalHoras80 += horasExtras;
+    document.getElementById("valorGuardado").textContent = "Valor guardado: " + horasExtras + " horas";
+    document.getElementById("borrar").style.display = "inline";
+    
+  } else {
+    alert("Ya existe un valor guardado para este día.");
+  }
+}
+
+
+
